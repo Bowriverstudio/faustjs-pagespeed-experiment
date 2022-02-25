dd72fb7c9f60c8340c133133f4e1583c9a6dc2b5
Maurice Tadros <maurice@bowriverstudio.com>
2022-02-25 09:54:22 -0700

    Temporarly Ignored ESlinter issues

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-const { withFaust } = require('@faustjs/next');
+const { withFaust } = require("@faustjs/next");
 
 /**
  * @type {import('next').NextConfig}
  **/
-module.exports = withFaust();
+module.exports = withFaust({
+  eslint: {
+    // This will be removed once there is a fix for ESLint: https://github.com/yannickcr/eslint-plugin-react/issues/3215
+    ignoreDuringBuilds: true,
+  },
+});
